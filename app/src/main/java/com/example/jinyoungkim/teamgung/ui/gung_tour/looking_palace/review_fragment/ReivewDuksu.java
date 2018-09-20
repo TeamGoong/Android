4fead46cc76d22b5e3cf5d44eedb8a775b0f91e6
suzzang <soochoi0325@gmail.com>
2018-09-21 03:20:41 +0900

    궁궐투어 - 알아보기부분 탭 구현완료

---
+++ app/src/main/java/com/example/jinyoungkim/teamgung/ui/gung_tour/looking_palace/review_fragment/ReivewDuksu.java
@@ -0,0 +1,21 @@
+package com.example.jinyoungkim.teamgung.ui.gung_tour.looking_palace.review_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jinyoungkim.teamgung.R;
+
+public class ReivewDuksu extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_review_duksu,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/jinyoungkim/teamgung/ui/gung_tour/looking_palace/viewpager_column_items/FirstColumn.java
@@ -0,0 +1,17 @@
+package com.example.jinyoungkim.teamgung.ui.gung_tour.looking_palace.viewpager_column_items;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jinyoungkim.teamgung.R;
+
+public class FirstColumn extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.vpitem_column1, container, false);
+    }
+}
