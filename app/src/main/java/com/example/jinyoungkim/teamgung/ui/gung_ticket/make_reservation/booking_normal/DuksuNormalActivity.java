fd2ae2cf378514cf8936716acf38599d90fcb8db
김진영 <gam0za@naver.com>
2018-09-23 21:57:27 +0900

    예매하기 뷰

---
+++ app/src/main/java/com/example/jinyoungkim/teamgung/ui/gung_ticket/make_reservation/booking_normal/DuksuNormalActivity.java
@@ -0,0 +1,15 @@
+package com.example.jinyoungkim.teamgung.ui.gung_ticket.make_reservation.booking_normal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jinyoungkim.teamgung.R;
+
+public class DuksuNormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duksu_normal);
+    }
+}
